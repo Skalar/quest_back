e24e034c4ab2531461c120d8da0a3fafbe307788
Thorbjørn Hermansen <thhermansen@gmail.com>
2014-02-18 20:24:41 +0100

    Added small helper method for building correct SOAP body.

--- lib/quest_back/api.rb
+++ lib/quest_back/api.rb
@@ -43,19 +43,48 @@ module QuestBack
     end
 
 
-    # Public: Deep merges default attributes to send with the SOAP request.
+
+
+    private
+
+
+    # Private: Deep merges default attributes to send with the SOAP request.
     #
     # Returns a merged hash for Savon client
     def build_hash_for_savon_call(attributes = {})
-      defaults = {
-        user_info: {
-          'Username' => config.username,
-          'Password' => config.password
-        }
-      }
+      defaults = hash_representation_of :user_info, {username: config.username, password: config.password}
 
       {
         message: defaults.deep_merge(attributes)
+      }
+    end
+
+
+    # Private: Duplicate some text an arbitrary number of times.
+    #
+    # Takes a type and it's attrs. Type should be underscored symbol, while attrs can be a
+    # hash with underscored symbols.
+    #
+    # Since QuestBack's API needs to have elements like this:
+    #
+    # <wsdl:TestConnection>
+    #   <wsdl:userInfo>
+    #     <wsdl:Username>
+    #     ...
+    #
+    # We cannot simply use Savon's convert_request_keys_to config, as it translate all keys.
+    # We need some keys camelcased and some lower_camelcased.
+    #
+    # Thus we map our inner attributes, for instance for userInfo to camelcase and keeps them
+    # as strings so Savon will not manipulate them.
+    #
+    # I guess this helper method here is kinda not optimal, and we may have a simple class / struct
+    # which can do this job for us, so the api class does not have multiple responsibilites. Oh well,
+    # works for now.
+    #
+    def hash_representation_of(type, attrs)
+      {
+        type => Hash[attrs.map { |k,v| [k.to_s.camelcase, v] }]
       }
     end
   end
