81a6f2acae224bd2be9976d14b51aaa29bbef959
Thorbjørn Hermansen <thhermansen@gmail.com>
2014-02-19 13:11:17 +0100

    Rewrote how we transform hash to make Savon build correct body.
    This is a more generic approach which we can give
    a hash to. It will traverse the hash and manipulate
    nested hashes.

--- lib/quest_back/api.rb
+++ lib/quest_back/api.rb
@@ -11,6 +11,8 @@ module QuestBack
       @config = attributes[:config]
     end
 
+
+
     def test_connection
       client.call :test_connection, build_hash_for_savon_call
     end
@@ -52,18 +54,16 @@ module QuestBack
     #
     # Returns a merged hash for Savon client
     def build_hash_for_savon_call(attributes = {})
-      defaults = hash_representation_of :user_info, {username: config.username, password: config.password}
+      defaults = {user_info: {username: config.username, password: config.password}}
+      message = defaults.merge attributes
 
       {
-        message: defaults.deep_merge(attributes)
+        message: transform_hash_for_quest_back(message)
       }
     end
 
 
-    # Private: Duplicate some text an arbitrary number of times.
-    #
-    # Takes a type and it's attrs. Type should be underscored symbol, while attrs can be a
-    # hash with underscored symbols.
+    # Private: Transforms given hash as how Savon needs it to build the correct SOAP body.
     #
     # Since QuestBack's API needs to have elements like this:
     #
@@ -73,7 +73,7 @@ module QuestBack
     #     ...
     #
     # We cannot simply use Savon's convert_request_keys_to config, as it translate all keys.
-    # We need some keys camelcased and some lower_camelcased.
+    # We need some keys camelcased (keys within nested hashes) and some lower_camelcased (keys in the outer most hash).
     #
     # Thus we map our inner attributes, for instance for userInfo to camelcase and keeps them
     # as strings so Savon will not manipulate them.
@@ -81,11 +81,15 @@ module QuestBack
     # I guess this helper method here is kinda not optimal, and we may have a simple class / struct
     # which can do this job for us, so the api class does not have multiple responsibilites. Oh well,
     # works for now.
-    #
-    def hash_representation_of(type, attrs)
-      {
-        type => Hash[attrs.map { |k,v| [k.to_s.camelcase, v] }]
-      }
+    def transform_hash_for_quest_back(hash, transform_keys = false)
+      Hash[
+        hash.map do |key, value|
+          key = transform_keys ? key.to_s.camelcase : key
+          value = value.is_a?(Hash) ? transform_hash_for_quest_back(value, true) : value
+
+          [key, value]
+        end
+      ]
     end
   end
 end
